58db505e4c8970b7d78f7122715f210d2fd70d73
D11 <email.fivemin@gmail.com>
2025-02-06 02:31:57 +0330

    u1

---
+++ index.js
@@ -0,0 +1,11 @@
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    return new Response("Hello World2!");
+  }
+};
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
+
